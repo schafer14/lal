b05855a584b801007413c813cac080d1db769b64
Banner B. Schafer <banner.schafer@uwa.edu.au>
2012-11-12 09:33:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lal::Application.config.secret_token = '6472389dc1f92c3f0f9fbaf0d036c93b2c12675299f8e911f5ea4b0159f684b8f5ea2f13c5e81b1b4ea8c930c7e09b33cd81258c7e4dfa2ef6cb8176c7f709a5'
